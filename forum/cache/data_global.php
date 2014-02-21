3ec893bedc6ae39a88129e326e68a619ae6bf9b1
MaydarakZakaria <zakaria.maydarak@utt.fr>
2014-02-21 11:14:58 +0100

    Jfusion parametré
    jfusion parametré ainsi que le plugin phpbb3 (slave/master),
    authentification,...

--- forum/cache/data_global.php
+++ forum/cache/data_global.php
@@ -3,3 +3,13 @@
 32
 mysqli_version
 s:10:"5.6.12-log";
+1424512826
+87
+imageset_site_logo_md5
+a:1:{s:11:"prosilver::";s:32:"0c461a32cd3621643105f0d02a772c10";}
+1393063234
+88
+versioncheck
+s:68:"3.0.12
+https://www.phpbb.com/community/viewtopic.php?f=14&t=2200921
+";
---
+++ logs/error.php
@@ -0,0 +1,16 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-02-21 09:46:43 UTC
+#Software: Joomla Platform 11.4.0 Stable [ Brian Kernighan ] 03-Jan-2012 00:00 GMT
+
+#Fields: date	time	priority	clientip	category	message
+2014-02-21	09:46:43	INFO	127.0.0.1	 FAILURE: 	Invalid password
+2014-02-21	09:46:55	INFO	127.0.0.1	 FAILURE: 	Invalid password
+2014-02-21	09:47:14	INFO	127.0.0.1	 FAILURE: 	Invalid password
+2014-02-21	09:47:55	INFO	127.0.0.1	 FAILURE: 	Invalid password
+2014-02-21	09:48:14	INFO	127.0.0.1	 FAILURE: 	User does not exist
+2014-02-21	09:48:47	INFO	127.0.0.1	 FAILURE: 	Invalid password
+2014-02-21	09:49:12	INFO	127.0.0.1	jfusion CANCELED: 	
+2014-02-21	09:58:07	INFO	127.0.0.1	 FAILURE: 	Invalid password
+2014-02-21	09:58:21	INFO	127.0.0.1	jfusion CANCELED: 	
+2014-02-21	10:08:27	INFO	127.0.0.1	 FAILURE: 	Invalid password
