3823802043e6cbb8290274fbc63083809a05df02
MaydarakZakaria <zakaria.maydarak@utt.fr>
2014-02-21 15:58:21 +0100

    acymailing
    acymailing

---
+++ logs/error.php
@@ -0,0 +1,38 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-02-21 09:46:43 UTC
+#Software: Joomla Platform 11.4.0 Stable [ Brian Kernighan ] 03-Jan-2012 00:00 GMT
+
+#Fields: date	time	priority	clientip	category	message
+2014-02-21	09:46:43	INFO	127.0.0.1	 FAILURE: 	Invalid password
+2014-02-21	09:46:55	INFO	127.0.0.1	 FAILURE: 	Invalid password
+2014-02-21	09:47:14	INFO	127.0.0.1	 FAILURE: 	Invalid password
+2014-02-21	09:47:55	INFO	127.0.0.1	 FAILURE: 	Invalid password
+2014-02-21	09:48:14	INFO	127.0.0.1	 FAILURE: 	User does not exist
+2014-02-21	09:48:47	INFO	127.0.0.1	 FAILURE: 	Invalid password
+2014-02-21	09:49:12	INFO	127.0.0.1	jfusion CANCELED: 	
+2014-02-21	09:58:07	INFO	127.0.0.1	 FAILURE: 	Invalid password
+2014-02-21	09:58:21	INFO	127.0.0.1	jfusion CANCELED: 	
+2014-02-21	10:08:27	INFO	127.0.0.1	 FAILURE: 	Invalid password
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+2014-02-21	12:39:18	INFO	127.0.0.1	Joomla FAILURE: 	Invalid password
+2014-02-21	12:39:28	INFO	127.0.0.1	jfusion CANCELED: 	
+2014-02-21	12:39:53	INFO	127.0.0.1	jfusion CANCELED: 	
+2014-02-21	12:40:29	INFO	127.0.0.1	jfusion CANCELED: 	
+2014-02-21	12:40:45	INFO	127.0.0.1	jfusion CANCELED: 	
+2014-02-21	12:40:52	INFO	127.0.0.1	Joomla FAILURE: 	Invalid password
+2014-02-21	12:41:44	INFO	127.0.0.1	jfusion CANCELED: 	
+2014-02-21	12:56:52	INFO	127.0.0.1	 FAILURE: 	EMPTY_PASSWORD_NO_ALLOWED
+2014-02-21	12:57:02	INFO	127.0.0.1	Joomla FAILURE: 	Invalid password
+2014-02-21	12:57:11	INFO	127.0.0.1	jfusion CANCELED: 	
+=======
+>>>>>>> 3ec893bedc6ae39a88129e326e68a619ae6bf9b1
+=======
+>>>>>>> 3ec893bedc6ae39a88129e326e68a619ae6bf9b1
+=======
+>>>>>>> 3ec893bedc6ae39a88129e326e68a619ae6bf9b1
+=======
+>>>>>>> 3ec893bedc6ae39a88129e326e68a619ae6bf9b1
